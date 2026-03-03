8eab792c7f4c257b4c8c05624ff43cf9231eb993
Anjan <gowdanjan18@gmail.com>
2026-03-03 10:18:06 +0530

    added scanner

--- linearSearch.java
+++ linearSearch.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 public class linearSearch{
 
 public static void main(String[] args){
-int[] arr = new int[10];
+Scanner sc=new Scanner(System.in);
+int[] arr =  {10,23,4,5,6,7,8,14,45,65};
 
-arr={10,5,3,2,7,8,14,78,34,23};
+System.out.println("enter the target element");
+int target=sc.nextInt();
 
 
 }
