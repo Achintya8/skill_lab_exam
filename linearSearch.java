0c043f76c187727e4af1662faac407caaea5d149
Achintya <achintyak30@gmail.com>
2026-03-03 09:45:01 +0530

    almost done

--- linearSearch.java
+++ linearSearch.java
@@ -1,10 +1,18 @@
 public class linearSearch{
 
 public static void main(String[] args){
-int[] arr = new int[10];
+int[] arr = {10,23,4,5,6,7,8,14,45,65};
 
-arr={10,5,3,2,7,8,14,78,34,23};
-
+//arr=(10,5,3,2,7,8,14,78,34,23);
+int n = arr.length;
+int target = 14;
+for(int a=0;a<n;a++){
+if (arr[a] == target){
+System.out.println("Element found at :" + a);
+}else{
+System.out.println("Element not found");
+}
+}
 
 }
 
